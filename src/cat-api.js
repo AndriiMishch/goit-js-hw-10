fe7fe90b538dcba0c8a38b542146ea68a876d208
AndriiMishch <anderito02@gmail.com>
2023-08-04 18:22:30 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,116 +1,32 @@
-import SlimSelect from 'slim-select'
-import '../node_modules/slim-select/dist/slimselect.css'
+import axios from 'axios';
 
-export const selectEl = document.querySelector('.breed-select');
-const catInfoContainer = document.querySelector('.cat-info');
-const loaderEl = document.querySelector('.loader');
-const errorEl = document.querySelector('.error');
-const API_KEY = 'live_FNr6YurFuEurXkkPjWLsc9byNk6mZxIdl5DKUUfbdzuUvz76NvP82IQMrXcPePb1';
+const API_KEY =
+  'live_AYm3OFL5RsopcRxkS6vktyN1rahZysaNXWqUpYbk0YmAblViSMpWRz5uBAhpOooy';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
-export const BASE_URL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-const showLoader = () => {
-  loaderEl.style.display = 'block';
-};
-
-const hideLoader = () => {
-  loaderEl.style.display = 'none';
-};
-
-const showBreedSelect = () => {
-  selectEl.style.display = 'block';
-};
-
-const hideBreedSelect = () => {
-  selectEl.style.display = 'none';
-};
-
-const showCatInfo = () => {
-  catInfoContainer.style.display = 'block';
-};
-
-const hideCatInfo = () => {
-  catInfoContainer.style.display = 'none';
-};
-
-const showError = () => {
-  errorEl.style.display = 'block';
-};
-
-const hideError = () => {
-  errorEl.style.display = 'none';
-};
-
-export const fetchBreeds = () => {
-  showLoader();
-  hideBreedSelect();
-  hideCatInfo();
-  hideError();
-
-  fetch(`${BASE_URL}/breeds`)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
+export function fetchBreeds() {
+  return axios
+    .get(`${BASE_URL}/breeds`)
+    .then(response => {
+      return response.data;
     })
-    .then((data) => {
-      const catInfo = data
-        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-        .join(" ");
-
-      selectEl.insertAdjacentHTML("beforeend", catInfo);
-
-
-  
-    })
-    .catch((error) => {
-      console.log('Failure', error);
-      showError();
-    })
-    .finally(() => {
-      hideLoader();
-      showBreedSelect();
-      new SlimSelect({
-        select: '#single'
-      })
+    .catch(error => {
+      console.error('Error fetching breeds:', error);
+      throw error;
     });
-};
+}
 
-export const fetchCatByBreed = (breedId) => {
-  showLoader();
-  hideCatInfo();
-  hideError();
-  
-
-  fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
+export function fetchCatByBreed(breedId) {
+  const url = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+  return axios
+    .get(url)
+    .then(response => {
+      return response.data;
     })
-    .then((data) => {
-      if (data.length > 0) {
-        const catData = data[0];
-        const catImage = `<img src="${catData.url}" alt="Cat Image" width = 500>`;
-        const catInfo = `
-          <h2>${catData.breeds[0].name}</h2>
-          <p>Description: ${catData.breeds[0].description}</p>
-          <p>Temperament: ${catData.breeds[0].temperament}</p>
-        `;
-
-        catInfoContainer.innerHTML = catImage + catInfo;
-        showCatInfo();
-      } else {
-        showError();
-      }
-    })
-    .catch((error) => {
-      console.log('Failure', error);
-      showError();
-    })
-    .finally(() => {
-      hideLoader();
+    .catch(error => {
+      console.error('Error fetching cat:', error);
+      throw error;
     });
-};
+}
